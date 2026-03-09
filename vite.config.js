5b77f4efc0b86ddd97a35c27fca60430f3002d02
shch6rskz2001-boop <shch6rskz2001@gmail.com>
2026-03-09 13:10:57 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
-import glob from 'glob';
+import { sync } from "glob";
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 
@@ -7,11 +7,11 @@ export default defineConfig(({ command }) => {
   return {
     root: 'src',
     build: {
-      sourcemap: true,
+      sourcemap: true,git git add .
       outDir: '../dist',
       rollupOptions: {
         // Всі HTML-файли як точки входу
-        input: glob.sync('./src/*.html'),
+        input: sync('./src/*.html'),
         output: {
           // Винесення node_modules у окремий чанк
           manualChunks(id) {
@@ -29,7 +29,7 @@ export default defineConfig(({ command }) => {
       FullReload(['./src/**/**.html']),
     ],
     resolve: {
-      // Це допомагає Vite знаходити node_modules при імпорті flatpickr / izitoast
+      // Допомагає Vite знаходити node_modules при імпорті flatpickr / izitoast
       alias: {
         flatpickr: 'flatpickr/dist/flatpickr.js',
         izitoast: 'izitoast/dist/js/iziToast.js',
