830bf9340198cb3e84ae1dbcc5d2e93ec7d4b560
shch6rskz2001-boop <shch6rskz2001@gmail.com>
2026-03-09 13:25:15 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,39 +1,37 @@
 import { defineConfig } from 'vite';
-import { sync } from "glob";
+import { sync } from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 
-export default defineConfig(({ command }) => {
-  return {
-    root: 'src',
-    build: {
-      sourcemap: true,
-      outDir: '../dist',
-      rollupOptions: {
-        // Всі HTML-файли як точки входу
-        input: sync('./src/*.html'),
-        output: {
-          // Винесення node_modules у окремий чанк
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return 'vendor';
-            }
-          },
-          // Індивідуальні файли JS
-          entryFileNames: '[name].js',
+export default defineConfig({
+  root: 'src', // корінь проєкту
+  build: {
+    outDir: '../dist', // куди скласти збірку
+    sourcemap: true,
+    rollupOptions: {
+      // всі HTML-файли як точки входу
+      input: sync('./src/*.html'),
+      output: {
+        // винесення node_modules у окремий чанк
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
         },
+        // індивідуальні файли JS
+        entryFileNames: '[name].js',
       },
     },
-    plugins: [
-      injectHTML(),
-      FullReload(['./src/**/**.html']),
-    ],
-    resolve: {
-      // Допомагає Vite знаходити node_modules при імпорті flatpickr / izitoast
-      alias: {
-        flatpickr: 'flatpickr/dist/flatpickr.js',
-        izitoast: 'izitoast/dist/js/iziToast.js',
-      },
+  },
+  plugins: [
+    injectHTML(),
+    FullReload(['./src/**/**.html']), // live reload при зміні HTML
+  ],
+  resolve: {
+    alias: {
+      // щоб Vite міг знайти CSS та JS для flatpickr і iziToast
+      flatpickr: 'flatpickr/dist/flatpickr.js',
+      izitoast: 'izitoast/dist/js/iziToast.js',
     },
-  };
+  },
 });
